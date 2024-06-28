b51a32889643b2b00de50bfda819c7f941b5800d
38215290 <flor.dani.gutierrez.006@gmail.com>
2024-06-28 11:22:59 -0300

    modd

--- sql.sql
+++ sql.sql
@@ -1,3 +1,21 @@
+CREATE VIEW sales_region AS
+	SELECT 
+    region,  
+    SUM(total_revenue) AS total_sales_region
+FROM 
+    ventas
+GROUP BY 
+    region;
+    
+CREATE VIEW sales_category AS
+	SELECT 
+    product_category,  
+    SUM(total_revenue) AS total_sales_category
+FROM 
+    ventas
+GROUP BY 
+    product_category;
+-- propuesta de optimizacion--
 SELECT product_category, 
 	SUM(total_revenue) as total,
 	RANK() OVER (ORDER BY SUM(total_revenue) DESC) AS sales_rank
